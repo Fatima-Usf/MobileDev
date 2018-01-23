93887833a6445d2561c449c410d4b36761f45a47
Fatima-YF <f.ucf.eva@gmail.com>
2018-01-23 23:29:37 +0100

    second try; if we click for the second time on the button his value change from "get ur age" to "u just clicked me, look in the buttom to get ur age"
    Signed-off-by: Fatima-YF <f.ucf.eva@gmail.com>

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.fatima.firsttry;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,12 +17,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public void getage(View view) {
 
         //comme on veut recupérer l'année de naissance de user on doit déclarer l'id de l'input afin de pouvoir travailler avec par la suite
-        EditText txt= findViewById(R.id.txt);
-        int yearB = Integer.parseInt( txt.getText().toString());
-        int age=2017-yearB;
-        Toast.makeText(this, "your age is "+String.valueOf(age),Toast.LENGTH_LONG).show();
+        EditText txt = findViewById(R.id.txt);
+        int yearB = Integer.parseInt(txt.getText().toString());
+        int age = 2017 - yearB;
+        Toast.makeText(this, "your age is " + String.valueOf(age), Toast.LENGTH_LONG).show();
+
+
+        final Button busmit = findViewById(R.id.busmit);
+
+
+        busmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                busmit.setText("u clicked me,look in the buttom");
+            }
+        });
+
+
     }
+
 }
