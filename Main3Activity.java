fefb9029a0e79159f4f7f59d95048eef34b37603
Fatima-YF <f.ucf.eva@gmail.com>
2018-01-25 00:18:52 +0100

    add button back
    Signed-off-by: Fatima-YF <f.ucf.eva@gmail.com>

--- Main3Activity.java
+++ Main3Activity.java
@@ -1,7 +1,9 @@
 package com.example.fatima.firsttry;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
@@ -15,5 +17,11 @@ public class Main3Activity extends AppCompatActivity {
         String user = b.getString("username");
         String pass = b.getString("password");
         t1.setText("user: " + user + "\npassword: " + pass);
+    }
+
+    public void backk(View view) {
+
+        Intent myint = new Intent(this, Main2Activity.class);
+        startActivity(myint);
     }
 }
